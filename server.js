191e14e242f88a00169ad897515be47e55c4e0f6
Pawel <pawel@aidodys.com>
2019-03-05 16:13:27 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('custom-env').env();
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const path = require("path");
+
+const app = express();
+const port = process.env.NODE_PORT || 8080;
+
+// Serve any static files built by React
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
